cafed584f8335165a965b5c3aa21b195b3cb242a
maurochabrito <mauriciorochasic@gmail.com>
2022-12-05 18:06:58 -0300

    Implemented Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(10, "Maumau", "maurochabrito@github.com", new Date(), 0.0, obj);
+		System.out.println(seller);
 	}
 
 }
